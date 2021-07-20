f05406ea1627b2d7e9459b33149b6d256e2def0a
Fernando Armoa <fernandoarmoa@gmail.com>
2021-07-20 16:17:38 -0300

    Projeto iniciado, mostrando a mensagem Hello World

---
+++ Secao8GitHubProjetoTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secao8GitHubProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
